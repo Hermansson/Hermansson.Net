fc3f848351e0ddc987f5f039d342621c176a42bc
Rickard Hermansson <rickard_hermansson@live.com>
2020-08-02 20:10:06 +0200

    Initial commit setting folder structure

---
+++ Design Patterns/Observer Observable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Observer_Observable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
